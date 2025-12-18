34041eb2521021013749a248388d2b16a6e83b6a
Lilisp16 <lijhosa@gmail.com>
2025-12-18 16:20:44 -0500

    Enviando cambios al proyecto. Logica Finalizada!

--- src/Agenda.java
+++ src/Agenda.java
@@ -1,6 +1,155 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Agenda {
-    private List<Contacto> contactos;   //guarda todos los contactos que tiene la agenda
-    private int tamaño;                    //guarda el número maximo de contactos que puede tener la agenda.
+    private List<Contacto> contactos;      //guarda todos los contactos que tiene la agenda
+    private int tamano;                    //guarda el número maximo de contactos que puede tener la agenda.
+
+
+    //CONSTRUCTORES
+
+    public Agenda() {
+        this(10);
+    }
+
+    public Agenda(int tamano) {
+        this.contactos = new ArrayList<>();
+        this.tamano = tamano;
+    }
+
+
+    // GETTERS AND SETTERS
+    public List<Contacto> getContactos() {
+        return contactos;
+    }
+
+    public void setContactos(List<Contacto> contactos) {
+        this.contactos = contactos;
+    }
+
+    public int getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+
+
+
+
+    //METODOS
+    public boolean agenda_llena(){  //valida si la agenda esta llena
+        return contactos.size()>=tamano;
+    }
+
+
+    public int espacioLibre(){  //para saber cuanto espacio queda en la agenda
+        return tamano-contactos.size();
+    }
+
+
+    public  boolean existe_contacto(Contacto contacto){ //verifica si existe el contacto
+        return contactos.contains(contacto);
+    }
+
+
+
+     // ANDIR CONTACTO
+
+    public void anadir_contacto(Contacto contacto){
+        if(contacto.getNombre().isEmpty() || contacto.getApellido().isEmpty()){ //validamos si nombre y apellido estan vacios
+            System.out.println("llene todos los campos");
+            return;
+        }
+
+        if (agenda_llena()) { // verificamos si agenda esta llena
+            System.out.println("Agenda Llena. No se puede añadir más contactos.");
+            return;
+        }
+
+        if (existe_contacto(contacto)){ // si el contacto existe devuelve true
+            System.out.println("Contacto ingresado ya existe");
+            return;
+        }
+        contactos.add(contacto);//?}
+        System.out.println("contacto añadido espacio libre: " + espacioLibre());
+    }
+
+
+
+
+    //ELIMINAR CONTACTO
+
+    public void eliminarContacto(String nombre, String apellido) {
+
+        // Recorremos la lista de contactos usando índice para poder eliminar
+        for (int i = 0; i < contactos.size(); i++) {
+            Contacto c = contactos.get(i); // Obtenemos el contacto actual
+
+            // Comparamos el nombre y apellido ignorando mayúsculas - minúsculas
+            if (c.getNombre().equalsIgnoreCase(nombre) && c.getApellido().equalsIgnoreCase(apellido)) {
+                contactos.remove(i); // Eliminamos el contacto de la lista
+                System.out.println("Contacto eliminado");
+                return; // Salimos del método, ya encontramos y eliminamos el contacto
+            }
+        }
+
+        // no se encontró ningún contacto con ese nombre y apellido
+        System.out.println("Contacto no existe");
+    }
+
+
+
+    //BUSCAR CONTACTO
+    public void buscarContacto(String nombre,String apellido){
+        for(Contacto contacto:contactos){
+
+            if(contacto.getNombre().equalsIgnoreCase(nombre) && contacto.getApellido().equalsIgnoreCase(apellido)){
+                System.out.println("Telefono " + contacto.getTelefono());
+                return;
+            }
+        }
+        System.out.println("contacto no encontrado");
+    }
+
+
+
+
+    //LISTAR CONTACTO
+    public  void listarContactos(){
+        if (contactos.isEmpty()) {  //valida si la agenda esta vacia
+            System.out.println("📭 La agenda está vacía.");
+            return;
+        }
+
+        // Ordenar alfabéticamente
+        Collections.sort(contactos, Comparator
+                .comparing(Contacto::getNombre, String.CASE_INSENSITIVE_ORDER)
+                .thenComparing(Contacto::getApellido, String.CASE_INSENSITIVE_ORDER));
+
+        System.out.println("📒 Lista de contactos:");
+        for (Contacto c : contactos) {
+            System.out.println(c);
+        }
+    }
+
+
+
+
+  // MODIFICAR TELEFONO
+    public void modificarTelefono(String nombre,String apellido, String nuevoTelefono){
+        for(Contacto contacto:contactos){
+            if(contacto.getNombre().equalsIgnoreCase(nombre) && contacto.getApellido().equalsIgnoreCase(apellido)){
+                contacto.setTelefono(nuevoTelefono);
+                System.out.println("Se actualizo el numero");
+                return;
+            }
+
+        }
+        System.out.println("contacto no encontrado");
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,109 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); //leemos las entradas del usuario
+        Agenda ag = new Agenda(); // Creamos la agenda con variable llamada ag
 
-        System.out.printf("Hello and welcome!");
+        boolean salir = false;    // mientras salir sea false, el menú seguirá mostrándose
 
+        while (!salir) {
+            // Mostramos el menu de opciones
+            System.out.println("\n--- AGENDA DE CONTACTO ---");
+            System.out.println("1. Añadir contacto");
+            System.out.println("2. Listar contactos");
+            System.out.println("3. Buscar contacto");
+            System.out.println("4. Modificar teléfono");
+            System.out.println("5. Eliminar contacto");
+            System.out.println("6. Salir");
+            System.out.print("Elige una opción: ");
 
+            int opcion = scanner.nextInt(); // Leemos la opción del usuario
+            scanner.nextLine();             //Limpiar el salto de línea
+
+            switch (opcion) {
+                case 1:
+
+                    //  Añadir contacto
+                    System.out.print("Nombre: ");
+                    String nombre = scanner.nextLine();
+                    System.out.print("Apellido: ");
+                    String apellido = scanner.nextLine();
+
+                    // Validación del teléfono debe tener 10 dígitos y solo números
+                    String telefono;
+                    do {
+                        System.out.print("Ingrese un numero de 10 digitos: ");
+                        telefono = scanner.nextLine();
+                        if (telefono.length() != 10 || !telefono.matches("\\d+")) {
+                            System.out.println("Dato inválido. Debe tener 10 números.");
+                        } else {
+                            break;
+                        }
+                    } while (true);
+
+                    // Añadimos el contacto a la agenda
+                    ag.anadir_contacto(new Contacto(nombre, apellido, telefono));
+                    break;
+
+                case 2:
+                    //Listamos contactos
+                    ag.listarContactos();
+                    break;
+
+                case 3:
+                    //  Buscamos contacto
+                    System.out.print("Nombre: ");
+                    nombre = scanner.nextLine();
+                    System.out.print("Apellido: ");
+                    apellido = scanner.nextLine();
+                    ag.buscarContacto(nombre, apellido);
+                    break;
+
+                case 4:
+                    // Modificamos teléfono
+                    System.out.print("Nombre: ");
+                    nombre = scanner.nextLine();
+                    System.out.print("Apellido: ");
+                    apellido = scanner.nextLine();
+
+                    // Validación del nuevo teléfono
+                    do {
+                        System.out.print("Nuevo teléfono (10 dígitos): ");
+                        telefono = scanner.nextLine();
+                        if (telefono.length() != 10 || !telefono.matches("\\d+")) {
+                            System.out.println("Dato Inválido. Debe tener 10 numeros.");
+                        } else {
+                            break;
+                        }
+                    } while (true);
+
+                    // Modificamos el teléfono del contacto
+                    ag.modificarTelefono(nombre, apellido, telefono);
+                    break;
+
+                case 5:
+                    // Eliminar contacto
+                    System.out.print("Nombre: ");
+                    nombre = scanner.nextLine();
+                    System.out.print("Apellido: ");
+                    apellido = scanner.nextLine();
+
+                    // Eliminamos el contacto
+                    ag.eliminarContacto(nombre, apellido);
+                    break;
+
+                case 6:
+                    // Salir
+                    salir = true;
+                    System.out.println("¡Vuelve pronto!");
+                    break;
+
+                default:
+                    System.out.println("Opción Invalida");
+            }
+        }
+
+        scanner.close();
     }
 }
